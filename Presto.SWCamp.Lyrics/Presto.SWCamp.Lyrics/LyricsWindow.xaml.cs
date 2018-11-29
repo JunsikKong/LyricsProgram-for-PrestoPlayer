e6993aa6d40840caa15ebbb811a475e8c875953e
Administrator <Administrator@DESKTOP-KJS.local>
2018-11-29 20:13:31 +0900

    3줄가사 등장시 같은 인덱스의 리스트에 삽입

--- Presto.SWCamp.Lyrics/Presto.SWCamp.Lyrics/LyricsWindow.xaml.cs
+++ Presto.SWCamp.Lyrics/Presto.SWCamp.Lyrics/LyricsWindow.xaml.cs
@@ -24,7 +24,6 @@ namespace Presto.SWCamp.Lyrics
     {
         List<string> lyricList = new List<string>(); // lrc 파일에서 시간 뒤의 문구(가사)를 저장하는 리스트
         List<string> timeList = new List<string>(); // lrc 파일에서 시간을 저장하는 list(mm:ss.ff 형식의 시간을 (mm*60 + ss.ff)*1000 로 저장)
-        private const int MAX_BTYE = 1024;
 
         public LyricsWindow()
         {
@@ -36,37 +35,41 @@ namespace Presto.SWCamp.Lyrics
         {
             lyricList.Clear();
             timeList.Clear();
+
             var musicFileName = System.IO.Path.GetFileNameWithoutExtension(PrestoSDK.PrestoService.Player.CurrentMusic.Path);
             var lyricFilePath = System.IO.Path.GetDirectoryName(PrestoSDK.PrestoService.Player.CurrentMusic.Path) + @"\" + musicFileName + ".lrc";
             String[] lines = File.ReadAllLines(lyricFilePath, Encoding.Default); // 경로의 파일의 줄을 읽는다. 글자가 깨지는 경우가 있어서 인코딩 입력
 
             foreach (var line in lines)
             {
-                var m = line.Substring(1,2);
-                
-                if (Regex.IsMatch(m, @"\d\d"))
+                var m = line.Split('[')[1].Split(']')[0];
+                if (Regex.IsMatch(m, @"\d\d\:\d\d\.\d\d"))//m의 값이 2자리의 숫자일 경우
                 {
-                    //int s = int.Parse(line.Substring(1,9));
-                    timeList.Add(line.Substring(1, 9));
-                    lyricList.Add(line.Substring(10));
+                    if(timeList.Count>0 && timeList[timeList.Count-1]==m)
+                    {
+                        String s = lyricList[lyricList.Count-1];
+                        lyricList.RemoveAt(lyricList.Count-1);
+                        lyricList.Add(s + "___" +line.Substring(10));
+                    }
+                    else
+                    {
+                        timeList.Add(m);
+                        lyricList.Add(line.Substring(10));
+                    }
+                    
                 }
             }
 
+            
             String temp = "";
-            int i = 0;
             foreach(var x in lyricList)
             {
-                i++;
-                temp += x + " ";
-                if (i % 5 == 0) { temp += "\n"; }
+                temp += x + " \n";
             }
 
             tbk1.Text = musicFileName;
             tbk2.Text = lyricFilePath;
-
-
-            MessageBox.Show(timeList[0] + "\n" + lyricList[0]);
-
+            tbk3.Text = temp;
         }
     }
 }
