8fd8805e728f7e007bdf2ed0f61a7f875089113f
Administrator <Administrator@DESKTOP-KJS.local>
2018-11-29 15:46:00 +0900

    재생음악 변경시 파일명과 가사파일경로 출력

--- Presto.SWCamp.Lyrics/Presto.SWCamp.Lyrics/LyricsWindow.xaml.cs
+++ Presto.SWCamp.Lyrics/Presto.SWCamp.Lyrics/LyricsWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Presto.SDK;
+using System;
+using System.IO;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -19,9 +21,21 @@ namespace Presto.SWCamp.Lyrics
     /// </summary>
     public partial class LyricsWindow : Window
     {
+        List<string> lyricList = new List<string>();
+        List<int> timeList = new List<int>();
+
         public LyricsWindow()
         {
             InitializeComponent();
+            PrestoSDK.PrestoService.Player.StreamChanged += MusicChanged; //음악이 바뀔 때 해당 프로시저 수행
+        }
+
+        private void MusicChanged(object sender, EventArgs e) //음악이 바뀔 때 수행되는 프로시저
+        {
+            var musicFileName = System.IO.Path.GetFileNameWithoutExtension(PrestoSDK.PrestoService.Player.CurrentMusic.Path);
+            var lyricFilePath = System.IO.Path.GetDirectoryName(PrestoSDK.PrestoService.Player.CurrentMusic.Path) + "\\" + musicFileName + ".lrc";
+            tbk1.Text = musicFileName;
+            tbk2.Text = lyricFilePath;
         }
     }
 }
